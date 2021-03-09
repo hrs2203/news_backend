0a615128619538b41e7acb13a2c219af5e655600
hrs2203 <hrishabh2203@gmail.com>
2021-03-09 17:47:56 +0530

    User Layer integrated

--- router/user.js
+++ router/user.js
@@ -1,26 +1,78 @@
 const router = require('express').Router();
+const parseJsonInput = require("body-parser").json();
+
+const UserDB = require("../database/db/user_module.js");
+const UserDetailDB = require("../database/db/user_detail_module.js");
+const { response } = require('express');
 
 
-router.post("/auth/login", (req, res, next) => {
-    return res.json({
-        "statusCode": 200,
-        "data": "/auth/login"
+router.post("/auth/login",
+    parseJsonInput,
+    (req, res, next) => {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        return UserDB.validateUser(email, password)
+            .then(validationResp => {
+                return res.json({
+                    "statusCode": 200,
+                    "data": validationResp
+                });
+            }).catch(err => {
+                return res.json({
+                    "statusCode": 400,
+                    "data": err
+                });
+            });
     });
-});
 
-router.post("/auth/registration", (req, res, next) => {
-    return res.json({
-        "statusCode": 200,
-        "data": "/auth/registration"
-    });
-});
+router.post("/auth/registration", parseJsonInput,
+    (req, res, next) => {
+        const username = req.body.username;
+        const email = req.body.email;
+        const password = req.body.password;
 
-router.get("/", (req, res, next) => {
-    return res.json({
-        "statusCode": 200,
-        "data": `userDetail ${req.query.uid}`
+        return UserDB.createNewUser(username, email, password)
+            .then(creationResp => {
+                return res.json({
+                    "statusCode": 200,
+                    "data": creationResp
+                });
+            }).catch(err => {
+                return res.json({
+                    "statusCode": 400,
+                    "data": err
+                });
+            })
     });
-});
+
+router.get("/",
+    (req, res, next) => {
+        const userEmail = req.query.email;
+
+        return UserDB.checkUserPresence(userEmail)
+            .then(userData => {
+                if (userData === false){
+                    return res.json({
+                        "statusCode": 200,
+                        "data": null
+                    });
+                }
+                return res.json({
+                    "statusCode": 200,
+                    "data": userData
+                });
+            })
+            .catch(err => {
+                return res.json({
+                    "statusCode": 400,
+                    "data": err
+                });
+            })
+
+
+        
+    });
 
 
 module.exports = router;
